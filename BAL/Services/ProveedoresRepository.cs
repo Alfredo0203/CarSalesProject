318a1ff9a886e464323801d58e2087aae3eb9ec1
veronica <Vero>
2021-09-02 21:40:11 -0600

    Método mostrar proveedores

---
+++ BAL/Services/ProveedoresRepository.cs
@@ -0,0 +1,34 @@
+﻿using BAL.IServices;
+using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAL.Services
+{
+    public class ProveedoresRepository : IDisposable, IProveedoresRepository
+    {
+        private readonly Contexto contexto;
+
+        //Constructor
+        public ProveedoresRepository(Contexto _contexto)
+        {
+            this.contexto = _contexto;
+        }
+
+        //Método para retornar listado de proveedores
+        public List<tabProveedores> listaProveedores()
+        {
+            var proveedores = contexto.tabProveedores.ToList();
+            return (proveedores);
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ SistemaInventario/Controllers/ProveedoresController.cs
@@ -0,0 +1,28 @@
+﻿using BAL.IServices;
+using BAL.Services;
+using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SistemaInventario.Controllers
+{
+    public class ProveedoresController : Controller
+    {
+        private IProveedoresRepository proveedoresRepository;
+
+        public ProveedoresController()
+        {
+            this.proveedoresRepository = new ProveedoresRepository(new Contexto());
+        }
+        
+        //Método para mostrar proveedores
+        public ActionResult MostrarProveedores()
+        {
+            var proveedores = proveedoresRepository.listaProveedores();
+            return View(proveedores);
+        }
+    }
+}
